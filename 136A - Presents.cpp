ac9aad6777914689b22eec72ccea2ca0299cd365
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-07-03 14:10:21 +0530

    Create 136A - Presents.cpp

---
+++ 136A - Presents.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int i,n,a[101],p;
+int main(){
+    cin>>n;
+    for(i=1;i<=n;a[p]=i,i++)
+        cin>>p;
+    for(i=1;i<=n;i++)
+        cout<<a[i]<<" ";
+return 0;
+}
