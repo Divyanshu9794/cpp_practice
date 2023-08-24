f22b14c3ad4882cb4abcacd7996734f4e9b2b9ad
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-08-24 16:32:15 +0530

    .

---
+++ undirected_weighted_graph.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+const  int v=5;
+
+int main(){
+    int graph[v][v];
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            graph[i][j]=0;
+
+        }
+    }
+    int e;
+    cout<<"Enter the number of edges"<<endl;
+    cin>>e;
+    for(int i=0;i<e;i++){
+        int s,d,wt;
+        cin>>s>>d>>wt;
+        graph[s][d]=wt;
+        graph[d][s]=wt;
+    }
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            cout<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+        
+    }
+
+}
