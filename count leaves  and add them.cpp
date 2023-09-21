aad12a16b24e6ce4f88cb345054ec31301a65a67
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-09-21 19:23:09 +0530

    .

---
+++ count leaves  and add them.cpp
@@ -0,0 +1,132 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+int countLeaves(struct Node* root);
+int sumLeaf(struct Node* root);
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		cout<< countLeaves(root)<<endl;
+        cout<< sumLeaf(root)<<endl;
+    }
+    return 0;
+}
+
+
+
+void inorder(Node* root,int &c){
+      if(root==NULL){
+          return;
+      }
+      inorder(root->left,c);
+      if(root->left==NULL && root->right==NULL){
+          c++;
+      }
+      inorder(root->right,c);
+  }
+int countLeaves(Node* root)
+{
+  int c=0;
+  inorder(root,c);
+  return c;
+  
+  
+}
+
+
+int sumLeaf(Node* root)
+{
+    int s=0;
+    if(root==NULL)
+    return 0;
+    else if(root->left==NULL && root->right==NULL)
+    return root->data;
+    else
+    s = sumLeaf(root->left)+sumLeaf(root->right);
+    return s;
+}
