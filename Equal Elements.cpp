1ae1af82a2e3a1d0355568bb8ebc5952b0b51435
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-07 20:07:58 +0530

    .

--- Equal Elements.cpp
+++ Equal Elements.cpp
@@ -1,25 +1,44 @@
 
 #include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
 using namespace std;
 
-int main() {
+int minOperation(int arr[], int n) {
 	
-	return 0;
+	unordered_map<int, int> hash;
+	for (int i=0; i<n; i++) {
+		if (hash.find(arr[i]) != hash.end()) {
+			hash[arr[i]]++;
+		} else {
+			hash[arr[i]] = 1;
+		}
+	}
+	
+	
+	int max_count = 0;
+	for (auto it : hash) {
+		max_count = max(max_count, it.second);
+	}
+	
+	
+	return (n - max_count); 
 }
 
-int minOperation(int arr[], int n) {
-    
-    unordered_map<int, int> hash;
-    for (int i=0; i<n; i++) {
-        if (hash.find(arr[i]) != hash.end()) {
-            hash[arr[i]]++;
-        } else {
-            hash[arr[i]] = 1;
+
+int main() {
+	int t;
+    while(t--){
+        int n ;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
         }
+        cout << minOperation(arr, n) << endl;
+
     }
-    int max_count = 0;
-    for (auto it : hash) {
-        max_count = max(max_count, it.second);
-    }
-    return (n - max_count); 
+	
+	return 0;
 }
