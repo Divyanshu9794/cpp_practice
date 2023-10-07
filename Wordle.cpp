af010b5d8a7433b17a399b0e4a1b1220f220ade3
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-07 19:50:55 +0530

    .

---
+++ Wordle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string a,b,t;
+        cin>>a>>b;
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b[i]){
+                t+='G';
+            }
+            else{
+                t+='B';
+            }
+        }
+        cout<<t<<endl;
+
+    }
+}
