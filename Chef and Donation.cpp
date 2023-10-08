6e1132c096751611a7c182ce41916d972b147e69
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-09 00:16:58 +0530

    .

--- Chef and Donation.cpp
+++ Chef and Donation.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<y-x<<endl;
+    }
 	
 	return 0;
 }
