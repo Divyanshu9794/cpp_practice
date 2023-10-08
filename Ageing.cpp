973502a059f76de4291692efc5f46b4c1f6a68eb
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-08 23:46:44 +0530

    .

---
+++ Ageing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+	    int n,m ;
+	    cin>>n;
+	    int a=n-10;
+	    std::cout << a << std::endl;
+    }
+	return 0;
+}
