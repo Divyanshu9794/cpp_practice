4268ceaf8ce22ff2e5a6d088acfea6878fd010e3
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-08 23:08:23 +0530

    .

---
+++ 2000RIP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	    int n ;
+	    cin>>n;
+	    int a=n*4;
+	    std::cout << a << std::endl;
+	
+	return 0;
+}
