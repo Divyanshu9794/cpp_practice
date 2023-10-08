7152b3dbb4dd0e0abe1120aa70381897fb73c14a
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-08 23:36:28 +0530

    .

---
+++ CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	    int n ;
+	    cin>>n;
+	    int a=n*2;
+	    std::cout << a << std::endl;
+	
+	return 0;
+}
