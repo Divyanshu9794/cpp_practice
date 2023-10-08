d9ec6cf617707eefff45439639fe951d0fcca289
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-08 23:45:29 +0530

    .

---
+++ Counting Words.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+	    int n,m ;
+	    cin>>n>>m;
+	    int a=n*m;
+	    std::cout << a << std::endl;
+    }
+	return 0;
+}
