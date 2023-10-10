a4166c38496c6572fad3b9402d303c445dcc0828
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-10 20:03:13 +0530

    .

---
+++ Chess Time.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int a=x*60;
+        cout<<a/20<<endl;
+    }
+}
