cd2c85ca94584cca9cfefbf9ac4a96d39549b820
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-11 18:46:53 +0530

    .

---
+++ Passes for Fair.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x+1<=y){
+            cout<<"Yes"<<endl;
+
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
