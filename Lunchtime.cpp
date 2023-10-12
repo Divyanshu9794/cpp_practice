3399cafe4c232ec2c952016c65a46344b86cdd1b
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-12 16:32:14 +0530

    .

---
+++ Lunchtime.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n>=1 && n<=4){
+            cout<<"YEs"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
