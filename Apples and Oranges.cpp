5d3e8be35e2c20969b6f14b9616cacb95b21c48c
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-12 16:35:51 +0530

    .

---
+++ Apples and Oranges.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a,b;
+    cin>>a>>b;
+    if(a+b<=n){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    
+}
