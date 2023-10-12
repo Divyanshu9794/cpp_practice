d1a6f710e8572908a3fc90d3db9f9f33082937b1
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-12 16:26:12 +0530

    .

---
+++ MATH1 Enrolment.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(y-x>0){
+            cout<<y-x<<endl;
+        }
+        else{
+            cout<<0<<endl;
+        }
+    }
+}
