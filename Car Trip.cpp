c59514abed5948e44c4ca5c85913be207cd1c6dd
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-17 19:41:02 +0530

    .

---
+++ Car Trip.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<=300){
+            cout<<3000<<endl;
+
+        }
+        else{
+            cout<<n*10<<endl;
+        }
+    }
+}
