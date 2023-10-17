4f0d02b2d2133481a39df24ffb50a02cbe9595c3
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-17 19:54:08 +0530

    .

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x*3<=y){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
