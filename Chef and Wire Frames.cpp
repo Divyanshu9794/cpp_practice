d4d84205b87184499fa7e06202729ea73a2b031c
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-18 09:30:05 +0530

    .

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        cout<<2*(n+x)*y<<endl;
+    }
+}
