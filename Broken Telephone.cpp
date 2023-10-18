ace1db7cdfc3ed9e5f223f6019642792bb34a345
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-18 10:15:37 +0530

    .

--- Broken Telephone.cpp
+++ Broken Telephone.cpp
@@ -12,16 +12,17 @@ int main(){
             cin>>a[i];
         }
         int ans=0;
-        if(a[0]!=a[1]){
-            ans++;
-        }
-        if(a[n]!=a[n-1]){
-            ans++;
-        }
+        
         for(int i=1;i<n-1;i++){
             if(a[i]!=a[i-1] || a[i]!=a[i+1]){
                 ans++;
             }
+        }
+        if(a[0]!=a[1]){
+            ans++;
+        }
+        if(a[n-1]!=a[n-2]){
+            ans++;
         }
         cout<<ans<<endl;
     }
