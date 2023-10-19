ed8145024a577751cefcf30fd81f6c3012caba99
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-19 19:01:34 +0530

    .

---
+++ Fill the Bucket.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<x-y<<endl;
+    }
+    
+}
