b328a9af28fa7451cd13ae39a69f2cac25bca31a
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-20 19:00:36 +0530

    .

---
+++ Tyre problem.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b;
+        cin>>a>>b;
+        cout<<(a*2+b*4)<<endl;
+        
+    }
+    return 0;
+}
+
