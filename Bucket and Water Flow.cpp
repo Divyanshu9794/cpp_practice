a6bf1ff0e44b00593229b7e5d967c713d9ec3bad
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-24 09:11:35 +0530

    .

---
+++ Bucket and Water Flow.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a+c*d>b){
+            cout<<"Overflow"<<endl;
+        }
+        else if(a+c*d<b){
+            cout<<"Unfilled"<<endl;
+        }
+        else{
+            cout<<"Filled"<<endl;
+        }
+    }
+}
