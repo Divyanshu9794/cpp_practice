f709d598a6f0c976a7e63c5267df4b82e34085c0
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-24 09:22:17 +0530

    .

---
+++ Playlist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int tim=3*x;
+	    cout<<n/tim<<endl;
+	}
+	return 0;
+}
