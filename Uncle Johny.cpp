63b3b2df9c86cd620adda721c60c826d9377df29
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-27 10:56:52 +0530

    .

---
+++ Uncle Johny.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int k;
+        cin>>k;
+        int x = a[k-1];
+        sort(a,a+n);
+        for(int i=0;i<n;i++){
+            if(a[i]==x){
+                cout<<(i+1)<<endl;
+                break;
+            }
+        }
+
+
+
+    }
+}
