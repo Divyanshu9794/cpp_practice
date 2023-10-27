78bfd73737c37b8d91cc7d7e09faf1121e8d81ec
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-27 10:14:19 +0530

    .

---
+++ Chef and Lockout Draws.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a[3];
+        for(int i=0;i<3;i++){
+            cin>>a[i];
+        }
+        sort(a,a+3);
+        if(a[2]==a[0]+a[1]){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+}
