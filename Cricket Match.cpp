e4c30f0a341ecf3431bd35b8ba59b0113c82396f
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-27 11:03:42 +0530

    .

---
+++ Cricket Match.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(b*36>=a){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+}
