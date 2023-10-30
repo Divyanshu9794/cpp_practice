8512f2914c479067ed12fba3b7bcbde8f3596802
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-30 18:34:35 +0530

    .

---
+++ CRED Coins.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        /* code */
+        int a,b;
+        cin>>a>>b;
+        int n = a*b;
+        cout<<(ceil(n/100))<<endl;
+        
+    }
+    
+
+}
