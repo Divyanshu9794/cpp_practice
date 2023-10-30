76f7e9aa08549871254eadb742a7d3057e1e8f00
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-10-30 18:24:37 +0530

    .

---
+++ Chef in his Office.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        /* code */
+        int a,b;
+        cin>>a>>b;
+        cout<<(a*4+b)<<endl;
+        
+    }
+    
+
+}
