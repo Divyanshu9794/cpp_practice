1453e5fdda59b7edebc2df02035c174a16c3e2ae
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-11-08 20:17:08 +0530

    .

---
+++ Pending Assignments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a*b)<=(c*24*60)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
