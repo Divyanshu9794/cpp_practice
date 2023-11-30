3c1a67c444c3f01cba623058ee5829fc8d3af74d
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-11-30 10:39:28 +0530

    .

--- Trade Surplus.cpp
+++ Trade Surplus.cpp
@@ -5,6 +5,13 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int a,b,
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if((a-b)+(b-c)<0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
     }
 }
