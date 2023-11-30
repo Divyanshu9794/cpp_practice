e7b1fc47f62382eb52690e8a416b908c823a34ba
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-11-30 08:53:31 +0530

    .

---
+++ It is My Serve.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+	    int x, y, s;
+	    cin>>x>>y;
+	    s = (x+y)%4;
+	    
+	    switch (s) {
+	        case 0:
+	        case 1:
+	            cout<<"Alice"<<endl;
+	            break;
+	        case 2:
+	        case 3:
+	            cout<<"Bob"<<endl;
+	    }
+	}
+	return 0;
+}
