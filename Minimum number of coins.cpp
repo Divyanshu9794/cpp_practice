65871025b55c69d4328a0582545108aff9c30a26
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-03 10:51:16 +0530

    .

---
+++ Minimum number of coins.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+cin>>t;
+while(t--)
+{
+    int x;
+    cin>>x;
+    if(x%5==0)
+    {
+        int p=x/10;
+        if(x==p*10)
+        {
+            cout<<p<<endl;
+        }
+        else{
+            cout<<p+1<<endl;
+        }
+        
+    }
+    else{
+        cout<<"-1\n";
+    }
+}
+	
+	return 0;
+}
