d999cf780b9489a4f5572762e1d1a6595f1355c3
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-20 20:18:12 +0530

    .

---
+++ Make n Zero.cpp
@@ -0,0 +1,20 @@
+# include <bits/stdc++.h> 
+# include <cmath> 
+using namespace std; 
+ 
+// # define ll long long 
+// # define setbits(x) __builtin_popcountll(x) 
+// # define mod 1000000007 
+
+int main() {
+  long long t; cin>>t;
+  while(t--){
+    long long n; cin>>n;
+    if(n == 1 ||  n==2|| n== 5){
+      cout<<"NO"<<endl;
+    }
+    else{
+      cout<<"YES"<<endl;
+    }
+  }
+}
