07652c1b8a34a4d35e0c7034b52d09724b0056c5
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-27 20:43:50 +0530

    .

---
+++ Painting Walls.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while (t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int a = x*y;
+        z=z/2;
+        cout<<z/a<<endl;
+    }
+    
+	
+
+}
