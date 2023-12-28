759df842a6808991185acdd8fd32590200973d1b
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-28 21:20:38 +0530

    .

--- E. Romantic Glasses.cpp
+++ E. Romantic Glasses.cpp
@@ -1,33 +1,44 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define ll long long
 
-using namespace std;
-
-void solve(){
-   
-   ll n,a,sum=0;
-   cin>>n;
-   for(int i=0;i<n;i++){
-       cin>>a;
-       sum+=a;
-   }
-   
-   ll k=sqrt(sum);
-   if(k*k==sum){
-       cout<<"YES";
-   }else{
-       cout<<"NO";
-   }
-    
-}
-
-int main() {
-   
-   int t=1;
-   cin>>t;
-   while(t--){
-       solve();
-       cout<<"\n";
-   }
-    return 0;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        int ans = 0;
+        map<ll, ll> m;
+        ll a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        m[0] = 1;
+        ll sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum = sum + a[i];
+            }
+            else
+            {
+                sum = sum - a[i];
+            }
+            if (m[sum])
+                ans = 1;
+            else
+                m[sum]++;
+        }
+        if (ans)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
 }
