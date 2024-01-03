ee3426642b58734a617d8927b0020142a482269d
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2024-01-03 20:08:22 +0530

    .

---
+++ Codechef Round.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%7==4){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
