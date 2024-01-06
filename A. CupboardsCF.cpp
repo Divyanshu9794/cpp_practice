2730969803cf4e5e72f0442dba6c99b00febae6e
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2024-01-06 10:34:06 +0530

    .

--- A. CupboardsCF.cpp
+++ A. CupboardsCF.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main(){
-    int n;
-    ci
+    int t;
+    cin >> t;
+    vector<vector<int>> a(t, vector<int> (2));
+    for(int i = 0; i < t; i++){
+        for(int j = 0; j < 2; j++){
+            cin >> a[i][j];
+        }
+    }
+    int l = 0, r = 0;
+    for(int i = 0; i < t; i++){
+        if(a[i][0] == 1) l += 1;
+    }
+    for(int i = 0; i < t; i++){
+        if(a[i][1] == 1) r += 1;
+    }
+    cout << min(l, t - l) + min(r, t - r) << endl;
+
 
 }
