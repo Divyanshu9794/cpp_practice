517138f85fe39694aecd3085340e59087ff0bd4c
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2024-01-07 09:28:46 +0530

    .

--- A. Nearly Lucky NumberCF.cpp
+++ A. Nearly Lucky NumberCF.cpp
@@ -1,1 +1,22 @@
-#include
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    string s = to_string(n);
+    int size= s.size();
+    int a=0;
+    for(int i=0;i<size;i++){
+        if(s[i]=='4' || s[i]=='7'){
+            a++;
+        }
+        
+    }
+    if(a==4 || a==7){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
