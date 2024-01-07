18c26e77c42832251c52a8257b7ac63df95c97ea
Divyanshu9794 <85441744+Divyanshu9794@users.noreply.github.com>
2024-01-08 00:30:46 +0530

    .

--- A. Stones on the TableCF.cpp
+++ A. Stones on the TableCF.cpp
@@ -2,3 +2,17 @@
 using namespace std;
 
 int main(){
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    int c=0;
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1]){
+            c++;
+        }
+    }
+    cout<<c<<endl;
+
+
+}
