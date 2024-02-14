a7612efada685cfb73b27a36032f84cb64665ced
“Divyanshu9794” <sdivyanshu352.com>
2024-02-14 21:01:02 +0530

    .

---
+++ Anti Adjacent Swaps.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        if(n==2){
+            if(arr[0]>arr[1]){cout<<"NO"<<endl;}
+            else{cout<<"YES"<<endl;}
+            continue;
+        }
+        if(n==3){
+            if(arr[1]<min(arr[2],arr[0]) || arr[1]>max(arr[2],arr[0])){cout<<"NO"<<endl;}
+            else
+                cout<<"YES"<<endl;
+                
+            continue;
+        }
+        
+        cout<<"YES"<<endl;
+    }
+}
