6fc27dc817d2b8b57369fd11f9b965bb0af8024f
“Divyanshu9794” <sdivyanshu352.com>
2024-02-16 15:19:21 +0530

    .

--- A. Football.cpp
+++ A. Football.cpp
@@ -3,5 +3,24 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
+    vector<string> s(n);
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+
+    }
+    int c=0;
+    int ans;
+    for(int i=0;i<n;i++){
+        if (count(s.begin(), s.end(), s[i]) > c) {
     
+                c = count(s.begin(), s.end(), s[i]);
+                ans = i;
+            }
+        }
+    
+ 
+    cout<<s[ans] << endl;
+
 }
