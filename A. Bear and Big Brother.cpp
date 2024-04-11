2228750e0600747a543ea0b23a432e2ab2088437
“Divyanshu9794” <sdivyanshu352.com>
2024-04-11 10:06:26 +0530

    .

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int c=0;
+    while(a<=b){
+        a=a*3;
+        b=b*2;
+        c++;
+    }
+    cout<<(c)<<endl;
+}
