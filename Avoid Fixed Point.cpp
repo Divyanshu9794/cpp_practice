596cd8b0428ddb32ca1f3e1110dc56d8daf08811
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-28 08:57:03 +0530

    .

---
+++ Avoid Fixed Point.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    int count=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(i+1+count==a[i])
+	        {
+	            count++;
+	        }
+	    }
+	cout<<count<<endl;
+	    
+	}
+	return 0;
+}
