1eb18cee6617b12ced8594efe10e48d0fe9d402d
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-07 20:21:14 +0530

    .

---
+++ Chef and Memory Limit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    long int sum=a[0];
+	  
+        for(long int  i=0;i<n-1;i++){
+	       if(a[i+1]>a[i]){
+	           sum+=a[i+1]-a[i];
+	       }
+	    }
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
