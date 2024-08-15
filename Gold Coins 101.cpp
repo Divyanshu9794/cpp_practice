9bf36ee16d60289678a2b081b5353774b5eab0e0
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-15 11:03:16 +0530

    .

---
+++ Gold Coins 101.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, X, Y;
+    cin >> A >> B >> X >> Y;
+
+    if (X > Y) {
+        cout << A << endl;  
+    } else {
+        cout << B << endl;
+    }
+
+    return 0;
+}
