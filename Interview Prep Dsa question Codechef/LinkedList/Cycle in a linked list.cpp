2e40fd9ce4e3785f4b3ab2dcdec3bb066ece9547
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-10-03 15:24:57 +0530

    .

---
+++ Interview Prep Dsa question Codechef/LinkedList/Cycle in a linked list.cpp
@@ -0,0 +1,64 @@
+// You are given a linked list 
+// A
+// A of size 
+// N
+// N.
+
+// Return the node where the cycle begins in the linked list. If there is no cycle, return NULL.
+
+// Input:
+// First line will contain 
+// T
+// T, number of test cases. Then the test cases follow.
+// Each test case contains three lines of input.
+// First line contains an integer 
+// N
+// N, length of the linked list 
+// A
+// A.
+// Second line contains 
+// A
+// 1
+// ,
+// A
+// 2
+// ,
+// …
+// A
+// N
+// A 
+// 1
+// ​
+//  ,A 
+// 2
+// ​
+//  ,…A 
+// N
+// ​
+//  , the value of the linked list nodes starting from the head for the linked list.
+// Third line contains an integer denoting the index of the node where the cycle starts.
+// Note:
+
+// For Java language, you need to:
+// Complete the function in the submit solution tab:
+
+// Node detectCycle(Node head){...}
+ 
+ 
+
+// For C++ language, you need to:
+// Complete the function in the submit solution tab:
+
+// Node* detectCycle(Node* head){...}
+ 
+ 
+
+// For Python language, you need to:
+// Complete the function in the submit solution tab:
+
+// def detectCycle(head):
+// Output:
+// The function you complete should return the required answer.
+
+
+
